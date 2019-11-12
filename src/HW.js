4862ac80f612f8c70f301559cd8369578b974ca3
Teddy Hidalgo <paulteddyhidalgo@gmail.com>
2019-11-11 19:33:15 -0800

    6 : Hello World

---
+++ src/HW.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function HW() {
+    return (
+        <div className="HW">
+            <h1>Hello World!</h1>
+        </div>
+        );
+    }
+
+export default HW;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import HW from './HW';
 import * as serviceWorker from './serviceWorker';
 
+// ReactDOM.render(What do you want to render, where do you want to render it)
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// Hello World
+// ReactDOM.render(<HW />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
