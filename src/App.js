ae0880653e125c8f40d8505a7a0f72e41597e1e6
Teddy Hidalgo <paulteddyhidalgo@gmail.com>
2019-11-12 14:25:00 -0800

    11.12.19 Components Folder & Footer Element

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <h1></h1>
+      </nav>
+      <Footer />
     </div>
   );
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
+// Enables JSX
 import React from 'react';
+
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import HW from './HW';
-import UL from './UL';
-import MyInfo from './MyInfo';
+import HW from './components/HW';
+import UL from './components/UL';
+import MyInfo from './components/MyInfo';
 import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render(What do you want to render, where do you want to render it)
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // Hello World
 // ReactDOM.render(<HW />, document.getElementById('root'));
@@ -17,7 +19,7 @@ import * as serviceWorker from './serviceWorker';
 // ReactDOM.render(<UL />, document.getElementById('root'));
 
 // MyInfo Blurb
-ReactDOM.render(<MyInfo />, document.getElementById('root'));
+// ReactDOM.render(<MyInfo />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
