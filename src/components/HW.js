176b981dd1e22948178f2e8b52d06c5c75381385
Teddy Hidalgo <paulteddyhidalgo@gmail.com>
2019-11-14 14:35:44 -0800

    11.14 Greeting Machine

--- src/components/HW.js
+++ src/components/HW.js
@@ -3,6 +3,19 @@ import React from "react";
 function HW() {
     const firstName = 'Teddy'
     const lastName = 'Hidalgo'
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+
+    if (hours <12) {
+        timeOfDay = 'morning'
+    } else if (hours >= 12 && hours < 17) {
+        timeOfDay = 'afternoon'
+    } else if (hours > 17 && hours < 23) {
+        timeOfDay = 'evening'
+    } else {
+        timeOfDay = 'night'
+    }
 
     return (
         <div className="HW">
@@ -11,6 +24,7 @@ function HW() {
 
             {/* ES6 String Templating Syntax? */}
             <h1>Hello {`${firstName} ${lastName}`}!</h1>
+            <h2>Good {`${timeOfDay}`}!</h2>
 
             {/* <h1>Hello World!</h1> */}
         </div>
